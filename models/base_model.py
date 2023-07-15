311ea5a457b4abc3da76775e43d4ef0f9d457cdb
FabPaul <paulfopa0@gmail.com>
2023-07-15 09:18:09 +0100

    Attempted task 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,26 @@ class BaseModel:
     """
         Base class for other classes to inherit from
     """
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
         """
             Initialisation of public instance attributes
         """
+        if kwargs is not None and kwargs != {}:
+            """ This checks if kwargs is not None and if Kwargs is not an empty dictionary"""
+            for keys in kwargs:
+                if keys == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif keys == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at #We were asked to assign this with the current datetime. SO in that regards, it I think self.updated_at should be equal to datetime.now()
-        #self.updated_at = datetime.now() #This is what I mean
+                else:
+                    self.__dict__[keys] = kwargs[keys]
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
 
     def __str__(self):
@@ -41,5 +52,5 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         #Added the other attributes of this instance.
         obj_dict["__class__"] = type(self).__name__
-        obj_dict["created_at"] = obj_dict["created_at"].isoformat("%Y-%m-%dT%H:%M:%S.%f")
-        obj_dict["updated_at"] = obj_dict["uodated_at"].isoformat("%Y-%m-%dT%H:%M:%S.%f")
+        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = obj_dict["uodated_at"].isoformat()
