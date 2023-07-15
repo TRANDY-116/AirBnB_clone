b3dacbba3f883d611649219d7862e72426283a0c
TRANDY-116 <randyfun16@gmail.com>
2023-07-15 15:52:51 +0100

    Most started with the console and add some basic processors

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+   Command interpreter
+"""
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+        command processors
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """
+            Exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+            Exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+            Do nothing when an empty line is entered
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
