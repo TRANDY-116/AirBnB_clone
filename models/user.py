d106392b6d5e628bfc8d2a8d9f0db366443c0bc4
TRANDY-116 <randyfun16@gmail.com>
2023-07-16 05:15:52 +0100

    nothing new

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Subclass of BaseModel
+"""
+# import
+
+class User(BaseModel):
+    """
+        User class that inherits from Basemodel
+    """
+
+    def __init__(s
+
